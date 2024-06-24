2894a7baa2ada0e153aca99ad2f176ed532592eb
Tatyana404 <kot.kate2020@gmail.com>
2024-06-24 23:27:35 +0200

    move options

--- assets/ui/Settings/Settings.component.js
+++ assets/ui/Settings/Settings.component.js
@@ -1,3 +1,4 @@
+import { gridSizeOptions, pointsToWinOptions, pointsToLoseOptions } from "../../js/data/constants.js";
 import { getSettings, setSettings } from "../../js/data/state-manager.js";
 import { createElement } from "../../js/utils/createElement.js";
 
@@ -29,30 +30,6 @@ export function SettingsComponent() {
   };
 
   const handleSettingChange = () => setSettings(gridSizeSelect.value, pointsToWinSelect.value, pointsToLoseSelect.value);
-
-  const gridSizeOptions = [
-    { value: "4", innerText: "4x4" },
-    { value: "5", innerText: "5x5" },
-    { value: "6", innerText: "6x6" },
-    { value: "7", innerText: "7x7" },
-    { value: "8", innerText: "8x8" },
-  ];
-
-  const pointsToWinOptions = [
-    { value: "20", innerText: "20 pts" },
-    { value: "40", innerText: "40 pts" },
-    { value: "50", innerText: "50 pts" },
-    { value: "60", innerText: "60 pts" },
-    { value: "80", innerText: "80 pts" },
-  ];
-
-  const pointsToLoseOptions = [
-    { value: "5", innerText: "5 pts" },
-    { value: "10", innerText: "10 pts" },
-    { value: "15", innerText: "15 pts" },
-    { value: "20", innerText: "20 pts" },
-    { value: "25", innerText: "25 pts" },
-  ];
 
   const { settingWrapper: gridSizeWrapper, settingSelect: gridSizeSelect } = createSettingWrapper("Grid size", "grid-size", gridSizeOptions, gridSize, handleSettingChange);
   const { settingWrapper: pointsToWinWrapper, settingSelect: pointsToWinSelect } = createSettingWrapper("Points to win", "points-to-win", pointsToWinOptions, pointsToWin, handleSettingChange);
