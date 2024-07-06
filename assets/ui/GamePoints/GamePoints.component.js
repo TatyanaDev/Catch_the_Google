21fcdc8f92236947a1549487a5e13b66f4dabd8a
Tatyana404 <kot.kate2020@gmail.com>
2024-07-06 14:24:56 +0200

    delete function formatTime

--- assets/ui/GamePoints/GamePoints.component.js
+++ assets/ui/GamePoints/GamePoints.component.js
@@ -1,7 +1,6 @@
 import { PointComponent } from "../common/Point/Point.component.js";
 import { getPoints, getTime } from "../../js/data/state-manager.js";
 import { createElement } from "../../js/utils/createElement.js";
-import { formatTime } from "../../js/utils/formatTime.js";
 
 export function GamePointsComponent() {
   const container = createElement("article", { class: "game-points-wrapper" });
@@ -10,7 +9,13 @@ export function GamePointsComponent() {
   const { google, players } = getPoints();
   const time = getTime();
 
-  gamePointsContainer.append(PointComponent("Player 1", players[0].value, "./assets/icons/player-1.svg", "Player 1"), PointComponent("Player 2", players[1].value, "./assets/icons/player-2.svg", "Player 2"), PointComponent("Google", google, "./assets/icons/google.svg", "Google"), PointComponent("Time:", formatTime(time)));
+  const minutes = Math.floor(time / 60);
+  const secs = time % 60;
+
+  const formattedMinutes = minutes.toString().padStart(2, "0");
+  const formattedSeconds = secs.toString().padStart(2, "0");
+
+  gamePointsContainer.append(PointComponent("Player 1", players[0].value, "./assets/icons/player-1.svg", "Player 1"), PointComponent("Player 2", players[1].value, "./assets/icons/player-2.svg", "Player 2"), PointComponent("Google", google, "./assets/icons/google.svg", "Google"), PointComponent("Time:", `${formattedMinutes}:${formattedSeconds}`));
 
   container.append(gamePointsContainer);
 
--- assets/ui/common/Modal/Modal.component.js
+++ assets/ui/common/Modal/Modal.component.js
@@ -2,7 +2,6 @@ import { playAgain, getTime } from "../../../js/data/state-manager.js";
 import { createElement } from "../../../js/utils/createElement.js";
 import { ButtonComponent } from "../Button/Button.component.js";
 import { PointComponent } from "../Point/Point.component.js";
-import { formatTime } from "../../../js/utils/formatTime.js";
 
 export function ModalComponent(src, alt, titleModalInnerText, textModalInnerText, point) {
   const container = createElement("article", { class: "result-wrapper" });
@@ -16,8 +15,11 @@ export function ModalComponent(src, alt, titleModalInnerText, textModalInnerText
 
   const time = getTime();
 
+  const minutes = Math.floor(time / 60);
+  const secs = time % 60;
+
   modalDecoration.append(modalDecorationImg);
-  modalResult.append(PointComponent("Catch:", point), PointComponent("Time", formatTime(time, true)));
+  modalResult.append(PointComponent("Catch:", point), PointComponent("Time", `${minutes}m ${secs}s`));
   modalContainer.append(titleModal, textModal, modalResult, ButtonComponent("Play again", playAgain));
   modalWrapper.append(modalDecoration, modalContainer);
   container.append(modalWrapper);
